822810f7ff73b7d6175d890b26ccab98d8873beb
Blake Creasser <blakecreasser@aggienetwork.com>
2023-04-27 19:30:39 -0500

    commit tree 7d84c0bee0bf8eb8743260b7406a0fba61760a75

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,44 @@
+class SpicesController < ApplicationController
+
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        if spice
+            render json: spice, status: :created
+        end
+    end
+
+    def update
+        spice = find_spice
+        if spice
+            spice.update(spice_params)
+            render json: spice
+        else
+            render_not_found
+        end
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_spice
+        Spice.find_by(id: params[:id])
+    end
+
+    def render_not_found
+        render json: {error: "Spice not found"}, status: :not_found
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
